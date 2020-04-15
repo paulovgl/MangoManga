11c2de24ba37d59c4c34345c75bf50f247b42de7
Mateus Malveira <mateusmalveira@alu.ufc.br>
2020-04-15 11:34:22 -0300

    adicionando conexão webocket

--- src/core/endpoints/index.js
+++ src/core/endpoints/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const endpoint = 'http://192.168.40.112:3333' 
+export const endpoint = 'http://192.168.40.112:3333'
+
+export const websocketpoint = 'ws://192.168.40.112:3333'
